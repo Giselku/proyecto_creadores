4557895976a84466895b0900202ab3d08503d2b9
kenia1403 <50304436+kenia1403@users.noreply.github.com>
2020-05-07 22:50:40 -0500

    Add files via upload

---
+++ Artículos/src/main/java/com/nfsm/springboot/web/app/ArtículosApplication.java
@@ -0,0 +1,13 @@
+package com.nfsm.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtículosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtículosApplication.class, args);
+	}
+
+}
---
+++ Artículos/src/test/java/com/nfsm/springboot/web/app/ArtículosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfsm.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtículosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
