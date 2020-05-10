88abc809ef062229e691cea529196534670f160a
KeniaEchazarreta <kenia.echazarreta@cbtis72.edu.mx>
2020-05-09 21:38:34 -0500

    indexController

---
+++ Artículos/src/main/java/com/nfsm/springboot/web/app/controllers/indexController.java
@@ -0,0 +1,31 @@
+package com.nfsm.springboot.web.app.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nfsm.springboot.web.app.models.Productos;
+
+
+@Controller
+@RequestMapping("/app")
+
+public class indexController {
+	
+	@RequestMapping ("/listar")
+	public String listar(Model model) {
+		List<Productos> producto=new ArrayList<>();
+		model.addAttribute("titulo", "Lista de productos"); 
+		model.addAttribute("productos", producto);
+		
+		Productos productos=new Productos();
+		
+		return "listar";
+	}
+	
+}
